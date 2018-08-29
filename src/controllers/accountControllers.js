c3ee09936ff38e08823ee54a2de1f99921b0b6d3
nidexiaoye <354818545@qq.com>
2018-08-29 20:03:41 +0800

    完成了获取注册页面

--- src/controllers/accountControllers.js
+++ src/controllers/accountControllers.js
@@ -1,1 +1,17 @@
 // 处理业务逻辑的代码--具体服务器要响应什么数据给浏览器
+
+/**
+ * 
+ * 最终处理,返回登录页面给浏览器
+ */
+
+ const path = require("path")
+
+exports.getLoginPage = (req,res) => {
+    res.sendFile(path.join(__dirname,"../statics/views/login.html"))
+}
+
+// 最终处理,返回注册页面给浏览器
+exports.getRegisterPage = (req,res) => {
+    res.sendFile(path.join(__dirname,"../statics/views/register.html"))
+}
--- src/routers/accountRouter.js
+++ src/routers/accountRouter.js
@@ -1,1 +1,21 @@
 // 分开处理浏览器发送过来的请求
+
+// 1.导入express
+const express = require("express")
+const path = require("path")
+
+// 2.创建路由对象
+const accountRouter = express.Router()
+
+// 导入控制器
+const accountCTRL = require(path.join(__dirname,"../controllers/accountControllers.js"))
+
+// 3.处理具体请求
+// 获取登录页面的请求
+accountRouter.get("/login",accountCTRL.getLoginPage)
+
+// 获取注册页面的请求
+accountRouter.get("/register",accountCTRL.getRegisterPage)
+
+// 4.导出
+module.exports = accountRouter
